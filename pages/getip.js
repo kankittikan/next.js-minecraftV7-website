8a952276ceb13a1b60317361edb49c750bfa30bc
KittikanMAKPHON <Kittikan Makphon>
2023-03-19 17:53:52 +0700

    update

---
+++ pages/getip.js
@@ -0,0 +1,35 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>MC | Get Server IP</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className='bg-white px-10 md:px-20 lg:px-40'>
+        <section className='min-h-screen'>
+          <nav className='py-10 mb-12 flex justify-between'>
+            <a href='/'>
+              <h1 className='text-md font-burtons'>Minecraft Version 7</h1>
+            </a>
+          </nav>
+          <div className='text-center shadow-lg p-10 rounded-xl'>
+            <img className='mx-auto' src="/assets/images/minecraft_logo.png" width={100} height={100} />
+            <h3 className='text-lg font-medium pt-8 pb-2'>hnoonnahoh.trueddns.com:30811</h3>
+            <p>
+              Good Luck!!
+            </p>
+          </div>
+          <div className='text-center'>
+            <h3 className='text-3xl py-1 mt-5'>Contact Me</h3>
+            <h4>nongkan195@gmail.com</h4>
+          </div>
+        </section>
+
+      </main>
+    </>
+  )
+}
